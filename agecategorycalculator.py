27ef9ecaf349ca51b7b4a60312c64f3729b52704
Anass <146187264+bokettoo@users.noreply.github.com>
2023-11-01 12:36:53 +0100

    Add files via upload

---
+++ agecategorycalculator.py
@@ -0,0 +1,9 @@
+age= int(input("enter the age"))
+if age>6 and age<7 : 
+    print("poussin")
+elif age>8 and age<9 : 
+    print("pupille")
+elif age>10 and age<11 : 
+    print("minime")
+elif age>12 : 
+    print("cadet")    
